b9d1d3cc8d8f63f7472cfa9f6ec805fb72185b8e
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2021-07-21 15:51:22 +0700

    Add project files.

---
+++ QuickSort/QuickSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void QuickSort(int*& a, int l, int r)
+{
+	int i = l, j = r;
+	int p = a[(l + r) / 2];
+	while (i < j)
+	{
+		while (a[i] < p) i++;
+		while (a[j] > p) j--;
+		if (i <= j)
+		{
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}
+	if (i<r) QuickSort(a, i, r);
+	if (l<j) QuickSort(a, l, j);
+}
+int main()
+{
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 0;i < n;i++)
+		cin >> a[i];
+	QuickSort(a, 0, n - 1);
+	for (int i = 0;i < n;i++)
+		cout << a[i] << "\n";
+	return 0;
+}
